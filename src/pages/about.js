81ccbef38173ee80a4b6365863384059f6156222
Anh <addao823@gmail.com>
2021-05-20 14:02:50 -0700

    Adding content to HourLocation and About

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,50 @@ import Seo from "../components/seo"
 const AboutPage = () => (
   <Layout>
     <Seo title="About" />
-    <h1>About page placeholder</h1>
-    <p>Including introduction and detail for the restaurant</p>
+    <h1 class="my-5 text-center text-primary display-4 fw-bold text-uppercase">
+      About page title
+    </h1>
+    <section class="container-md fs-6 my-3 ">
+      <h2>Our Story</h2>
+      <article>
+        The Butcher’s Cut prime steakhouse & seafood opened its doors shortly
+        before Covid-19. Inspired California cuisine with this contemporary
+        grill and steakhouse is nested in the heart of the Gaslamp. The ambiance
+        is a rustic and vintage vibe that takes you back to a simply classic
+        era. The newly installed glass meat locker on the restaurant floor is a
+        spectacle one must experience in person. The Butcher’s Cut promises to
+        be the freshest, most desired dining destination in the Gaslamp
+        District. We are passionate about serving perfectly aged steaks,
+        locally-grown vegetables, and an award-winning wine list. These elements
+        have combined to provide our guests with a unique culinary experience.
+      </article>
+    </section>
+    <section class="container-md fs-6 my-3 ">
+      <h2>Our Menu</h2>
+      <article>
+        The menu showcases a veritable who’s who of meats in all forms, sourced
+        from the very best beef and lamb producers. Quality beef is carefully
+        selected from the highest pedigree and defined by breed and feed. You
+        will find dishes like the Signature Prime Rib, selected from the finest
+        150-day grain fed Black Angus that has been dry aged then patiently
+        roasted in their special oven. Or The Butcher Cut’s cut eye fillet on
+        the bone, farmed on the West Coast, which gives this animal an unmatched
+        tenderness and flavour that can only come from cattle fed on New
+        Zealand’s magnificent grasslands.
+      </article>
+    </section>
+    <section class="container-md fs-6 my-3 ">
+      <h2>A Boutique Dining Experience</h2>
+      <article>
+        Immerse yourself in our vast, sommelier-selected wine collection. Each
+        visit to The Butcher’s Cut takes you on a gastronomic adventure that
+        defines us not as a meal but as an experience. In short, whether you’re
+        looking for a casual dinner with friends or a sophisticated dinner with
+        clients, there’s something to suit every palate. So come on down to the
+        Gaslamp and enjoy life at The Butcher’s Cut. You’ll always be welcome
+        here!
+      </article>
+    </section>
   </Layout>
 )
 
--- src/pages/hour-and-location.js
+++ src/pages/hour-and-location.js
@@ -6,8 +6,31 @@ import Seo from "../components/seo"
 const HourLocationPage = () => (
   <Layout>
     <Seo title="Hour and Location" />
-    <h1>Hour and location page placeholder</h1>
-    <p>Including map, address, hour of operation</p>
+    <div class="container-md d-flex flex-column justify-content-center text-center">
+      <h1 class="display-3 text-uppercase fw-bolder text-primary">
+        Hours and Location
+      </h1>
+      <section class="fs-6 my-5">
+        <address>
+          <a
+            href="https://www.google.com/maps/place/The+Butcher's+Cut/@32.7121796,-117.1602808,21z/data=!4m8!1m2!3m1!2sThe+Butcher's+Cut!3m4!1s0x80d9535322443a11:0x26730f1397b244e9!8m2!3d32.712178!4d-117.1602846"
+            class="text-uppercase link-light"
+          >
+            <p>
+              644 Fifth Ave <br />
+              San Diego, CA 92101
+            </p>
+          </a>
+          <a href="tel:+16192358144" class="text-uppercase link-light">
+            <p>{`(619) 235 8144`}</p>
+          </a>
+        </address>
+      </section>
+      <section>
+        <p class="fs-4 text-primary fw-bold">Tuesday - Sunday:</p>
+        <p>4PM - 10PM</p>
+      </section>
+    </div>
   </Layout>
 )
 
