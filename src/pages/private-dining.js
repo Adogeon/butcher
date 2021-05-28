17115d0d70e7efb2b7cec51881fd25b6ae4bbee9
Anh <addao823@gmail.com>
2021-05-27 19:54:12 -0700

    Adding content for private-dining

--- src/pages/private-dining.js
+++ src/pages/private-dining.js
@@ -7,7 +7,46 @@ const PrivateDiningPage = () => (
   <Layout>
     <Seo title="Private Dining" />
     <h1>Private Dining placeholder</h1>
-    <p>Show detail about making a private dining reservation</p>
+    <article>
+      <p>
+        At Butcher’s Cut, our private wine cellar seats up to forty people
+        comfortably with the luxury of at-table service and an in-house
+        Sommelier. Our sommelier hand-selects wines from our notable wine list
+        that compliments each dish seamlessly. With our Wine Cellar you get your
+        own exclusive space that has a relaxing, romantic, vintage atmosphere.
+        The walls are draped with authentic, black and white butcher paintings
+        that will take you back to a simpler time. We offer a chef’s pick,
+        seasonal, pre-set menu for larger parties or a la carte as well. We
+        invite you to treat your staff or family and friends to a dining
+        experience you will cherish forever at the newest and trendiest
+        restaurant in all of the Gaslamp District.
+      </p>
+      <p>
+        The Butcher’s Cut Steakhouse, located in the heart of the Gaslamp
+        Quarter, is proud to offer its facility to the public for any daytime
+        function. Butcher’s Cut offers private dining experiences for corporate
+        lunches, business meetings or anything you desire in a gorgeous, rustic
+        setting. We offer private exclusive breakfast meetings or corporate
+        lunches with a fresh modern alternative to conventional San Diego
+        dining. Our seasonal innovative menu along with sophisticated service
+        will exceed all of your needs. Book your next private event with The
+        Butcher’s Cut.
+      </p>
+      <p>
+        Venue Rentals are available for groups seeking daytime meeting space.
+        Meeting Packages include:
+      </p>
+      <ul>
+        <li>Private Meeting Space to conduct your presentation</li>
+        <li>AV Rental (projector & screen)</li>
+        <li>
+          Continental Breakfast (may be upgraded to American Breakfast for an
+          additional cost)
+        </li>
+        <li>Hot Coffee Station</li>
+        <li>Rental Fees</li>
+      </ul>
+    </article>
   </Layout>
 )
 
